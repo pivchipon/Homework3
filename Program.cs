4c7ec4ba4ac757c478303d2d3deac191ce4d086b
Stepan <pivchipon.games@mail.ru>
2022-11-27 17:42:31 +0300

    added all ex

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+
+/*Console.WriteLine("Enter the five-digit number");
+string arr = Console.ReadLine();
+int Len = arr.Length;
+if (Len == 5 )
+{
+    if (arr[0] == arr[4] && arr[1] == arr[3])
+    {
+        Console.WriteLine($"{arr} - is a palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"{arr} - is not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine($"{arr} - is not a five-digit number");
+}
+Console.WriteLine();*/
+
+
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+/*Console.WriteLine("Enter x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+
+Console.WriteLine(result);*/
+
+
+/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+/*Console.WriteLine("Enter N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int count = 1;
+while (count < N)
+{
+    Console.WriteLine (Math.Pow(count,3));
+    count++;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
